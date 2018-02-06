b94009cc665e0d5ba510799c2ef27227e7d55a28
Maxim Sluysarenko <waspmax1@gmail.com>
2018-02-06 03:21:19 +0200

    Add files via upload

--- components/OneClickBuy.php
+++ components/OneClickBuy.php
@@ -5,13 +5,19 @@ namespace bl\preorder\components;
 use bl\preorder\common\models\Preorder;
 use yii\base\Widget;
 
-
+/**
+ * @author Maxim Sluysarenko <waspmax1@gmail.com>
+ *
+ * Class OneClickBuy
+ * @package bl\preorder\components
+ */
 class OneClickBuy extends Widget
 {
     public function init()
     {
         parent::init();
         PreorderAsset::register($this->getView());
+        $this->registerTranslations();
     }
 
     public function run()
@@ -20,6 +26,17 @@ class OneClickBuy extends Widget
         return $this->render('OneClickBuy', [
             'model' => $model
         ]);
+    }
+
+    public function registerTranslations()
+    {
+        if (!isset(\Yii::$app->i18n->translations['preorder'])) {
+            \Yii::$app->i18n->translations['preorder'] = [
+                'class'          => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath'       => '@vendor/black-lamp/yii2-preorder/components/messages',
+            ];
+        }
     }
 
 }
--- components/PreorderAsset.php
+++ components/PreorderAsset.php
@@ -4,16 +4,31 @@ namespace bl\preorder\components;
 
 use yii\web\AssetBundle;
 
+/**
+ * @author Maxim Sluysarenko <waspmax1@gmail.com>
+ *
+ * Class PreorderAsset
+ * @package bl\preorder\components
+ */
 class PreorderAsset extends AssetBundle
 {
+    /**
+     * @var array
+     */
     public $js = [
         'js/preorder-main.js',
     ];
 
+    /**
+     * @var array
+     */
     public $css = [
         'css/preorder-main.css',
     ];
 
+    /**
+     * @var array
+     */
     public $depends = [
         'yii\web\JqueryAsset',
         'yii\bootstrap\BootstrapAsset',
