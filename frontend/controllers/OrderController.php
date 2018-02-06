f27e1b56fcd746b6b3923084bdc00a19a4352b3d
Maxim Sluysarenko <waspmax1@gmail.com>
2018-02-06 03:23:01 +0200

    Add files via upload

--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -2,28 +2,28 @@
 
 namespace bl\preorder\frontend\controllers;
 
+use bl\preorder\frontend\models\Order;
 use Yii;
 use bl\preorder\common\models\Preorder;
-
 use yii\web\Controller;
 
+/**
+ * @author Maxim Sluysarenko <waspmax1@gmail.com>
+ *
+ * Class OrderController
+ * @package bl\preorder\frontend\controllers
+ */
 class OrderController extends Controller
 {
+    /**
+     * method called when adding a new order and triggering Order model events
+     */
     public function actionAddOrder() {
         $model = new Preorder();
+        $orderModel = new Order();
         if($model->load(Yii::$app->request->post()) && $model->save()){
-            $message = Yii::$app->mailer->compose();
-            if (Yii::$app->user->isGuest) {
-                $message->setFrom('no-reply@cartaterra.local');
-            } else {
-                $message->setFrom(Yii::$app->user->identity->email);
-            }
-            $message->setTo(Yii::$app->params['adminEmail'])
-                ->setSubject('Заказ товара')
-                ->setTextBody('Заказ товара ' . $model->description . ' от ' . $model->phone_number)
-                ->send();
-           Yii::$app->session->setFlash('confirmed');
-          $model->refresh();
+            $orderModel->on(Order::FORM_CONFIRMED, [$orderModel, 'formConfirm']);
+            $orderModel->trigger(Order::FORM_CONFIRMED);
         }
     }
 }
