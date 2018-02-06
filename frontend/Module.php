2448184bd2f510f5ba1c05bab732a7b9d88d609d
Maxim Sluysarenko <waspmax1@gmail.com>
2018-02-06 03:22:42 +0200

    Add files via upload

--- frontend/Module.php
+++ frontend/Module.php
@@ -2,6 +2,12 @@
 
 namespace bl\preorder\frontend;
 
+/**
+ * @author Maxim Sluysarenko <waspmax1@gmail.com>
+ *
+ * Class Module
+ * @package bl\preorder\frontend
+ */
 class Module extends \yii\base\Module
 {
     public function init()
